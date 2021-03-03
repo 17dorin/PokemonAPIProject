1a62f35a33a9d64db80e0e018a8b1228a341406b
Nick D'Oria <17dorin@gmail.com>
2021-03-03 16:34:30 -0500

    Added git ignore

---
+++ Models/PokemonDAL.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PokemonAPIProject.Models
+{
+    public class PokemonDAL
+    {
+        //This process looks close to the same for all Non-Javascript Languages
+        //1) Figure out your URL <- Keys also sometimes go into your URL
+        //2) Create an HTTP request 
+        //2.1) (if needed) special setup like keys and useragents go here
+        //3) Get Response from Request 
+        //4) Get Response Stream and feed it into a Stream Reader 
+        //5) Use the Stream Reader to grab the contents of the JSON 
+        //6) Most of the time, convert the JSON into a model 
+
+        //Changes per the API 
+        //7) How you use your model <-- Every model for every API will look different 
+
+        public string GetData(string pokemon)
+        {
+            //URL can be different based upon endpoint/API 
+            string url = $"https://pokeapi.co/api/v2/pokemon/{pokemon}/";
+
+            //Web Requests sometimes need Headers/User Agent prop
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string json = rd.ReadToEnd();
+            return json;
+
+            //else
+            //{
+            //    throw new Exception("Error: Subreddit Names must be between 3 to 21 characters");
+            //}
+
+            //catch (Exception e)
+            //{
+            //    return "Error:" + e.StackTrace;
+            //}
+
+
+        }
+
+        public PokemonRoot GetPokemon(string name)
+        {
+            string json = GetData(name);
+
+            PokemonRoot r = JsonConvert.DeserializeObject<PokemonRoot>(json);
+            return r;
+
+        }
+    }
+}
