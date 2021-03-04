4110f249b11b40e02c7a118195b48a64f46b3527
Nick D'Oria <17dorin@gmail.com>
2021-03-03 21:14:09 -0500

    Can search by move. Some moves break conversion, need to tweak MoveRoot object

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,31 @@ namespace PokemonAPIProject.Controllers
             return View(p);
         }
 
+        public IActionResult SearchByMove(string move)
+        {
+            //Normalizes search string
+            string search = move.Trim().ToLower();
+
+            //Deserializes move object
+            MoveRoot m = pk.GetMove(search);
+
+            //Storing user input to display in view
+            TempData["moveName"] = move;
+
+            List<PokemonRoot> pokemonByMove = new List<PokemonRoot>();
+
+            //Move objects have an array of pokemon that can learn the move, represented by a name string and a url string
+            foreach(Learned_By_Pokemon l in m.learned_by_pokemon)
+            {
+                //Uses each name in the GetPokemon method to deserialize into a PokemonRoot, adding them to a list
+                PokemonRoot p = pk.GetPokemon(l.name);
+                pokemonByMove.Add(p);
+            }
+
+            //Passing the list into the view
+            return View(pokemonByMove);
+        }
+
         public IActionResult Privacy()
         {
             return View();
